bee71e6586c422b05c53b9775adff37b65bb3358
Atozzio <winzzhhzzhh@163.com>
2018-02-08 00:04:14 +0000

    2.7

--- gui_new.py
+++ gui_new.py
@@ -5,32 +5,33 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.dropdown import DropDown
+from kivy.properties import NumericProperty,StringProperty
 
-class InterfaceApp(App):
+class Quant_Select_Dropdown(DropDown):
+    quant = NumericProperty(0)
+
+
+class GreyHatsApp(App):
     def __init__(self):
-        super(InterfaceApp,self).__init__()
+        super(GreyHatsApp,self).__init__()
 # use floatlayout for a prettier display 
     def build(self):
         layout = GridLayout(cols=1,row_force_default=True, row_default_height=30)
-        label = Label(text='How many objects do you want in the scene?')#,halign="left")
+        label = Label(text='How many objects do you want in the scene?')
         layout.add_widget(label)
         mainbutton = Button(text='Please choose', size_hint_x=None, width=300)
-        Quant_Select = DropDown()
-        for index in xrange(1,6):
-            btn = Button(text=str(index), size_hint_y=None, height=40)
-            btn.bind(on_release=lambda btn: Quant_Select.select(btn.text))
-            Quant_Select.add_widget(btn)
+        Quant_Select = Quant_Select_Dropdown()
+        print Quant_Select.quant
         mainbutton.bind(on_release=Quant_Select.open)
 
         Quant_Select.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
         layout.add_widget(mainbutton)
-        btn = Button(text='OK', size_hint_x=None, width=100)#,on_press=)
-        layout.add_widget(btn)
         return layout
 
 if __name__ == '__main__':
-    InterfaceApp().run()
+    GreyHatsApp().run()
 
 #获取另一个widget值,用kivy language  (root.label_text)
 # https://stackoverflow.com/questions/30202801/how-to-access-id-widget-of-different-class-from-a-kivy-file-kv
-#显示隐藏widget内容，当判断条件为真时，再add_widget
+# https://groups.google.com/forum/#!topic/kivy-users/dTuNe7Y_OpM
+#显示隐藏widget内容，当判断条件为真时，再add_widget,在greyhats这个class里面新建函数，调用
